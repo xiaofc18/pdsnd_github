aff50ec58a86f78e37855b62b9dc5cf51da11e47
Xiaofeng Cao <xiaofeng.cao@zalando.de>
2018-11-08 18:11:12 +0100

    add comment for filter function

--- bikeshare.py
+++ bikeshare.py
@@ -7,7 +7,8 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-#####################################
+              
+# define filter function
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
